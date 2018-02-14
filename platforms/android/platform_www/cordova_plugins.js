221d2af36409af2736295fd8b3303b0cf4341ac2
stevepodell <steve@podell.com>
2018-02-13 17:05:21 -0800

    iOS test build 6 -- Larger font on splash screen Build 6, contains all of the React from WebApp "Suppressed Facebook signin for WeVoteCordova  #1319" Feb 13, 2018 ~ 4:45pm

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -15,6 +15,14 @@ module.exports = [
     "clobbers": [
       "SafariViewController"
     ]
+  },
+  {
+    "id": "cordova-plugin-statusbar.statusbar",
+    "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+    "pluginId": "cordova-plugin-statusbar",
+    "clobbers": [
+      "window.StatusBar"
+    ]
   }
 ];
 module.exports.metadata = 
@@ -22,7 +30,8 @@ module.exports.metadata =
 {
   "cordova-plugin-whitelist": "1.3.3",
   "cordova-plugin-device": "2.0.1",
-  "cordova-plugin-safariviewcontroller": "1.5.2"
+  "cordova-plugin-safariviewcontroller": "1.5.2",
+  "cordova-plugin-statusbar": "2.4.1"
 };
 // BOTTOM OF METADATA
 });
