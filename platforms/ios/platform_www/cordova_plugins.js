4212f9057a15bdea91ff096004057d52ddf91cd4
stevepodell <steve@podell.com>
2019-06-17 11:50:50 -0700

    Added new icons for iOS and Android

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -41,20 +41,20 @@ module.exports = [
     ]
   },
   {
-    "id": "cordova-plugin-statusbar.statusbar",
-    "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
-    "pluginId": "cordova-plugin-statusbar",
-    "clobbers": [
-      "window.StatusBar"
-    ]
-  },
-  {
     "id": "cordova-plugin-inappbrowser.inappbrowser",
     "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
     "pluginId": "cordova-plugin-inappbrowser",
     "clobbers": [
       "cordova.InAppBrowser.open",
       "window.open"
+    ]
+  },
+  {
+    "id": "cordova-plugin-statusbar.statusbar",
+    "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+    "pluginId": "cordova-plugin-statusbar",
+    "clobbers": [
+      "window.StatusBar"
     ]
   }
 ];
@@ -66,9 +66,9 @@ module.exports.metadata =
   "cordova-plugin-keyboard": "1.2.0",
   "cordova-plugin-safariviewcontroller": "1.5.3",
   "cordova-plugin-splashscreen": "5.0.2",
-  "cordova-plugin-statusbar": "2.4.2",
   "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-inappbrowser": "3.0.0"
+  "cordova-plugin-inappbrowser": "3.0.0",
+  "cordova-plugin-statusbar": "2.4.2"
 };
 // BOTTOM OF METADATA
 });
